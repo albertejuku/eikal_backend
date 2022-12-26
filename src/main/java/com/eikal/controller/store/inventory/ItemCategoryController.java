41762e4a2c296e4835f3f6f36225432eaa2d93f8
kinyanjuiprince21 <kinyanjuiprince21@gmail.com>
2022-12-26 22:56:05 +0300

    Changes

--- src/main/java/com/eikal/controller/store/inventory/ItemCategoryController.java
+++ src/main/java/com/eikal/controller/store/inventory/ItemCategoryController.java
@@ -3,8 +3,7 @@ package com.eikal.controller.store.inventory;
 import com.eikal.models.store.inventory.ItemCategory;
 import com.eikal.service.store.inventory.ItemCategoryService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author David Kinyanjui
@@ -20,10 +19,19 @@ public class ItemCategoryController {
     }
 
     @PostMapping("itemCategory/add")
-    public ResponseEntity<?> addItemCategory(ItemCategory itemCategory) {
+    public ResponseEntity<?> addItemCategory(@RequestBody ItemCategory itemCategory) {
         ItemCategory addedItemCategory = itemCategoryService.addCategory(itemCategory);
         return addedItemCategory != null ?
                 ResponseEntity.status(201).body(addedItemCategory) :
                 ResponseEntity.status(415).build();
     }
+
+    @GetMapping("itemCategory/{id}")
+    public ResponseEntity<?> findCategory(@PathVariable Long Id) {
+        ItemCategory category = itemCategoryService.findCategoryById(Id);
+        return category != null ?
+                ResponseEntity.status(200).body(category) :
+                ResponseEntity.status(404).body("did not found category");
+    }
+
 }
