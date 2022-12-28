40e5917121eb0c902e1929ce985e6f7ed2f97c43
kinyanjuiprince21 <kinyanjuiprince21@gmail.com>
2022-12-28 12:48:47 +0300

    Changes

--- src/main/java/com/eikal/controller/store/shop/OrderedItemController.java
+++ src/main/java/com/eikal/controller/store/shop/OrderedItemController.java
@@ -1,6 +1,11 @@
 package com.eikal.controller.store.shop;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.eikal.error.GlobalError;
+import com.eikal.models.store.shop.OrderedItem;
+import com.eikal.service.store.shop.OrderedItemService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author David Kinyanjui
@@ -8,4 +13,52 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderedItemController {
+
+    private final OrderedItemService orderedItemService;
+
+    public OrderedItemController(OrderedItemService orderedItemService) {
+        this.orderedItemService = orderedItemService;
+    }
+
+    @PostMapping("orderedItem/add")
+    public ResponseEntity<?> addOrderedItem(@RequestBody OrderedItem orderedItem) {
+        OrderedItem addedOrderedItem = orderedItemService.addOrderedItem(orderedItem);
+        return addedOrderedItem != null ?
+                ResponseEntity.status(201).body(addedOrderedItem) :
+                ResponseEntity.status(400).body(new GlobalError((short) 400,"Ordered item not added"));
+    }
+
+    @PutMapping("orderedItem/update")
+    public ResponseEntity<?> updateOrderedItem(@RequestBody OrderedItem orderedItem,@RequestParam Long id) {
+        OrderedItem updatedOrderedItem = orderedItemService.updateOrderedItem(orderedItem,id);
+        return updatedOrderedItem != null ?
+                ResponseEntity.status(201).body(updatedOrderedItem) :
+                ResponseEntity.status(400).body(new GlobalError((short) 400,"Ordered item not updated"));
+    }
+
+    @GetMapping("orderedItem/{id}")
+    public ResponseEntity<?> findOrderedItemById(@PathVariable Long id) {
+        OrderedItem orderedItem = orderedItemService.findOrderedItemById(id);
+        return orderedItem != null ?
+                ResponseEntity.status(200).body(orderedItem) :
+                ResponseEntity.status(404).body(new GlobalError((short) 404,"Ordered item not found"));
+    }
+
+    @GetMapping("orderedItem/all")
+    public ResponseEntity<?> findAllOrderedItem(@RequestParam int page,@RequestParam int size) {
+        Page<OrderedItem> orderedItemPage = orderedItemService.findAllOrderedItems(page,size);
+        return ResponseEntity.status(200).body(orderedItemPage);
+    }
+
+    @GetMapping("orderedItem/salePrice")
+    public ResponseEntity<?> findBySalePrice(@RequestParam double salePrice,@RequestParam int page,int size) {
+        Page<OrderedItem> orderedItemPage = orderedItemService.findBySalePrice(salePrice,page,size);
+        return ResponseEntity.status(200).body(orderedItemPage);
+    }
+
+    @GetMapping("orderedItem/quantity")
+    public ResponseEntity<?> findByQuantity(@RequestParam int quantity,@RequestParam int page,@RequestParam int size){
+        Page<OrderedItem> orderedItemPage = orderedItemService.findByQuantity(quantity,page,size);
+        return ResponseEntity.status(200).body(orderedItemPage);
+    }
 }
--- src/main/java/com/eikal/controller/store/shop/PaymentController.java
+++ src/main/java/com/eikal/controller/store/shop/PaymentController.java
@@ -1,6 +1,11 @@
 package com.eikal.controller.store.shop;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.eikal.error.GlobalError;
+import com.eikal.models.store.shop.Payment;
+import com.eikal.service.store.shop.PaymentService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author David Kinyanjui
@@ -8,4 +13,64 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("payment/add")
+    public ResponseEntity<?> addPayment(@RequestBody Payment payment) {
+        Payment addedPayment = paymentService.addPayment(payment);
+        return addedPayment != null ?
+                ResponseEntity.status(201).body(addedPayment) :
+                ResponseEntity.status(400).body(new GlobalError((short) 400, "Payment was not added"));
+    }
+
+    @PutMapping("payment/update")
+    public ResponseEntity<?> updatePayment(@RequestBody Payment payment,@RequestParam Long id){
+        Payment updatedPayment = paymentService.updatePayment(payment,id);
+        return updatedPayment != null ?
+                ResponseEntity.status(201).body(updatedPayment) :
+                ResponseEntity.status(400).body(new GlobalError((short) 400,"Payment was not updated"));
+    }
+
+    @GetMapping("payment/all")
+    public ResponseEntity<?> findAllPayments(@RequestParam int page,@RequestParam int size) {
+        Page<Payment> paymentPage = paymentService.findAllPayments(page,size);
+        return ResponseEntity.status(200).body(paymentPage);
+    }
+
+    @GetMapping("payment/orderQuantity")
+    public ResponseEntity<?> findByOrderQuantity(@RequestParam int quantity,@RequestParam int page,@RequestParam int size) {
+        Page<Payment> paymentPage = paymentService.findByOrderQuantity(quantity,page,size);
+        return ResponseEntity.status(200).body(paymentPage);
+    }
+
+    @GetMapping("payment/orderCode")
+    public ResponseEntity<?> findByOrderCode(@RequestParam String code) {
+        Payment payment = paymentService.findByOrderCode(code);
+        return payment != null ?
+                ResponseEntity.status(200).body(payment) :
+                ResponseEntity.status(404).body(new GlobalError((short) 404,"Payment not found"));
+    }
+
+    @GetMapping("payment/customerEmail")
+    public ResponseEntity<?> findByOrderCustomerEmail(@RequestParam String email,@RequestParam int page,@RequestParam int size) {
+        Page<Payment> paymentPage = paymentService.findByOrderCustomerEmail(email,page,size);
+        return ResponseEntity.status(200).body(paymentPage);
+    }
+
+    @GetMapping("payment/deliveryAddress")
+    public ResponseEntity<?> findByOrderDeliveryAddress(@RequestParam String address,@RequestParam int page,@RequestParam int size) {
+        Page<Payment> paymentPage = paymentService.findByOrderDeliveryAddress(address,page,size);
+        return ResponseEntity.status(200).body(paymentPage);
+    }
+
+    @GetMapping("payment/status")
+    public ResponseEntity<?> findByStatus(@RequestParam String status,@RequestParam int page,@RequestParam int size) {
+        Page<Payment> paymentPage = paymentService.findByStatus(status,page,size);
+        return ResponseEntity.status(200).body(paymentPage);
+    }
 }
